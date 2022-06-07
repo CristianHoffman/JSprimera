0e60df20abc70713a3607154300f62b97514e357
Cristian Hoffman <cristian.hoffman99@gmail.com>
2022-06-07 15:10:57 -0300

    desafio dos

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-let moneda = prompt ("Que auto te compras? ingrese su monto")
-
-if ((moneda >=800) && (moneda <= 999)){
-    alert("Podes comprarte el auto azul")
-}else if ((moneda >=1000) && (moneda <1999)){
-    alert("Podes comprarte el auto rojo")
-}else if ((moneda >=2000) && (moneda <=2999)){
-    alert("Podes comprarte el blanco")
-}else{
-    alert("no podes comprarte ninguno")
-}
-
 //////////////algoritmo en ciclo////////////////
 
 let count = 0
@@ -29,11 +17,48 @@ for (let index = 0; index < 20; index++) {
 
 ////////clave con while////////
 
-let clave = prompt("ingrese su clave")
+// let clave = prompt("ingrese su clave")
 
-while (clave !== "asdasd") {
-    entrada = prompt("ingrese nuevamente la clave")
+// while (clave !== "asdasd") {
+//     entrada = prompt("ingrese nuevamente la clave")
     
 
+// }
+// alert("Correcto")
+
+//////////function///////////////////
+function indicarMoneda() {
+    
+  let moneda = prompt ("Que auto te compras? ingrese su monto")
+    
+    if (moneda >=800 && moneda <= 999){
+        alert("Podes comprarte el auto azul")
+    }else if (moneda >=1000 && moneda <1999){
+        alert("Podes comprarte el auto rojo")
+    }else if (moneda >=2000 && moneda <=2999){
+        alert("Podes comprarte el blanco")
+    }else{
+        alert("no podes comprarte ninguno")
+    }
 }
-alert("Correcto")
+
+indicarMoneda ()
+
+/////////////funciones parametros/////////////
+
+function resta(num1, num2 ) {
+    let res = num1 - num2;
+    document.write("el resultado es " + res)
+    
+}
+ resta (33, 12)
+
+ //////////funcion flecha ////////////
+
+ const saludo = (nombre) => { 
+    document.write("<br>")
+     document.write("Hola " + nombre)
+     
+ }
+
+ saludo("Facu")
